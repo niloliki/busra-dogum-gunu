539aad9d492742db199a1329dd58bd50b1db32b5
niloliki <nilaycftc1577@gmail.com>
2025-06-24 17:20:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function nextScene(sceneNumber) {
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById("scene" + i).style.display = "none";
+    }
+    document.getElementById("scene" + sceneNumber).style.display = "block";
+}
 
